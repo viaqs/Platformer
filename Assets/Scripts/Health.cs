de42a56a83bd48c91e1c488a28d47b19f1715f5d
*ੈ✩‧₊˚༺☆༻*ੈ✩‧₊˚ <kalantaitejustina@gmail.com>
2024-12-15 14:44:27 +0200

    update

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -1,51 +1,63 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using UnityEngine;
+using UnityEngine.Events;
 
 public class Health : MonoBehaviour
 {
+    public AudioClip damageSound;
+
     public int maxHealth = 6;
     public List<Image> hearts;
-    public Sprite Full;
-    public Sprite Half;
-    public Sprite Empty;
+    public Sprite fullHeart;
+    public Sprite halfHeart;
+    public Sprite emptyHeart;
+    public UnityEvent onDamage;
+    public UnityEvent onDeath;
+
     private int health;
+    private AudioSource audioSource;
 
     void Start()
     {
+        audioSource = GetComponent<AudioSource>();
         health = maxHealth;
-        
     }
 
     public void TakeDamage(int damage)
     {
+        audioSource.PlayOneShot(damageSound);
+
         health -= damage;
-        if(health < 0)
+        onDamage.Invoke();
+
+        if (health < 0)
         {
             health = 0;
-
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            onDeath.Invoke();
         }
+
         UpdateHearts();
+
+        //TODO: death event or reload
     }
-    
+
     void UpdateHearts()
     {
-        for(int i = 0; i < hearts.Count; i++)
+        for (int i = 0; i < hearts.Count; i++)
         {
-            if(health>=(i+1) * 2)
+            if (health >= (i + 1) * 2)
             {
-                hearts[i].sprite = Full;
+                hearts[i].sprite = fullHeart;
             }
-            else if(health == (1 * 2) + 1)
+            else if (health == (i * 2) + 1)
             {
-                hearts[i].sprite = Half;
+                hearts[i].sprite = halfHeart;
             }
             else
             {
-                hearts[i].sprite = Empty;
+                hearts[i].sprite = emptyHeart;
             }
         }
     }
